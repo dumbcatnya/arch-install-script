4d51da32172095e09d03b5875fc348aae4371cc8
rushi a <93713977+rushia272@users.noreply.github.com>
2022-04-16 16:28:29 +0200

    Add files via upload

--- arch-installer.sh
+++ arch-installer.sh
@@ -34,39 +34,8 @@ swapon /dev/$swappartition
 pacman -Syy
 mount /dev/$rootpartition /mnt
 
+wget https://raw.githubusercontent.com/rushia272/arch-install-script/main/run-in-chroot.sh
 pacstrap /mnt base linux linux-firmware sudo nano
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-
-cat /usr/share/zoneinfo/
-echo "select zone"
-read zone
-
-cat /usr/share/zoneinfo/$zone
-echo "select subzone"
-read subzone
-
-ln -sf /usr/share/zoneinfo/$zone/$subzone /etc/localtime
-
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_US ISO-8859-1" > /etc/locale.gen
-locale-gen
-
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-export LANG=en_US.UTF-8
-
-echo '$hostname' > /etc/hostname
-echo "127.0.1.1" '$hostname' >> /etc/hosts
-
-echo "set password for root"
-passwd 
-
-pacman -S grub efibootmgr os-prober mtools
-mkdir /boot/efi
-mount /dev/'$bootpartition' /boot/efi
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi
-grub-mkconfig -o /boot/grub/grub.cfg
-pacman -S git wget
-wget https://raw.githubusercontent.com/rushia272/arch-install-script/main/install-gnome.sh
-sh install-gnome.sh
+arch-chroot /mnt run-in-chroot.sh
 #end of script
