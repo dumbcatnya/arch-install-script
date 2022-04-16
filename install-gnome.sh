245415601bf2dc345b3898051190850c28219ab3
rushi a <93713977+rushia272@users.noreply.github.com>
2022-04-16 17:29:55 +0200

    Update install-gnome.sh

--- install-gnome.sh
+++ install-gnome.sh
@@ -1,17 +1,6 @@
 #!bin/sh
 pacman -S xorg-server xorg-apps
-echo "pick gpu"
-echo "1) nvidia"
-echo "2) amd"
-echo "3) intel"
-read gpudriver
-if  [$gpudriver == 1]; then 
-    pacman -S nvidia nvidia-utils
-elif [$gpudriver == 2]; then
-    pacman -S xf86-video-ati
-elif [$gpudriver == 3]; then
-    pacman -S xf86-video-intel
-else echo "type valid number" exit
+pacman -S nvidia nvidia-utils xf86-video-ati xf86-video-intel   
 pacman -S gnome gnome-extra networkmanager
 systemctl enable gdm 
 systemctl enable NetworkManager 
