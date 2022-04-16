dba7d7a3ad8f956ea926ff229a8b22eb20a4545c
rushi a <93713977+rushia272@users.noreply.github.com>
2022-04-16 16:46:08 +0200

    Update install-gnome.sh

--- install-gnome.sh
+++ install-gnome.sh
@@ -5,11 +5,11 @@ echo "1) nvidia"
 echo "2) amd"
 echo "3) intel"
 read gpudriver
-if  $gpudriver=1; then 
+if  [$gpudriver=1] then 
     pacman -S nvidia nvidia-utils
-elif $gpudriver=2 then
+elif [$gpudriver=2] then
     pacman -S xf86-video-ati
-elif $gpudriver=3 then
+elif [$gpudriver=3] then
     pacman -S xf86-video-intel
 else echo "type valid number" exit
 pacman -S gnome gnome-extra networkmanager
